bd99c4f507af4eef56793f6099372509ad9137b0
sebasarturo37 <sebasarturo37@hotmail.com>
2019-09-05 17:39:46 -0500

    v1.0

--- fibonaci.py
+++ fibonaci.py
@@ -1,3 +1,7 @@
+#developed by: Sebastián D. Arturo - Juan Sebastián Erazo
+#ingeniería de sistemas 10 A
+#Electiva profesional III - Frameworks
+
 def fibona(a):
     if a==0 or a==1:
         return 1
